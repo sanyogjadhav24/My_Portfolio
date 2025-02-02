22cad405243c6d9313f816b6aaa5b03d14ea360f
Sanyog Jadhav <jadhavsanyog.400@gmail.com>
2025-02-02 21:48:42 +0530

    remove nav

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 
 import HomePage from "@/components/home";
-import FloatingNavDemo  from "@/components/navbar";
+
  export default function Home() {
   return (
     <div>
-      <FloatingNavDemo />
+    
      <HomePage/>
     </div>
   );
