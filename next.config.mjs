2645e87afd54419453c680f019909239d607da45
Sanyog Jadhav <jadhavsanyog.400@gmail.com>
2025-02-02 21:40:48 +0530

    reomve() form home3

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true, // Helps identify issues in development
-    swcMinify: true, // Enables faster minification using SWC
-    compiler: {
-      styledComponents: true, // Support for styled-components (if used)
-    },
-    experimental: {
-      appDir: true, // Enables the new Next.js App Router (if using Next 13+)
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
