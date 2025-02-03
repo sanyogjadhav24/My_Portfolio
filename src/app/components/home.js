e3f2878bf72f8a45f228ece87a0571df90dcc711
Sanyog Jadhav <jadhavsanyog.400@gmail.com>
2025-02-03 19:46:06 +0530

    corrcct next

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -2,14 +2,14 @@
 import React from "react";
 
 // import {BackgroundBeamsDemo} from "./hero";
-// import Projects from "./projects";
-// import Education from "./education";
-// import Contact from "./contactme";
-// import Footer from "./footer";
+import Projects from "./projects";
+import Education from "./education";
+import Contact from "./contactme";
+import Footer from "./footer";
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-black-900 text-white">
- 
+      <Projects/>
      
     </div>
     
