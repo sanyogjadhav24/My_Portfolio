36ae5f64778fa05aa16ec1bb5fe77f6984a3ca0c
Sanyog Jadhav <jadhavsanyog.400@gmail.com>
2025-02-03 19:57:35 +0530

    change allthinsg

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-import BackgroundBeamsDemo from "./hero"; // Ensure the filename matches exactly
+//import BackgroundBeamsDemo from "./hero"; // Ensure the filename matches exactly
 import Projects from "./projects";
 import Education from "./education";
 import Contact from "./contactme";
@@ -11,19 +11,19 @@ const HomePage = () => {
   return (
     <div className="min-h-screen bg-black text-white">
       {/* Hero Section */}
-      <BackgroundBeamsDemo />
+      {/* <BackgroundBeamsDemo /> */}
 
       {/* Projects Section */}
       <Projects />
 
       {/* Education Section */}
-      {/* <Education /> */}
+      <Education />
 
       {/* Contact Section */}
-      {/* <Contact /> */}
+      <Contact />
 
       {/* Footer Section */}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
